f9f1fdd526425ef13914b912224b866a5b1e0410
Illia <student@epicodus.com>
2016-02-12 10:12:21 -0800

    add pizzaCost methode.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,33 @@ function Pizza (pizzaSize) {
   this.dressings = [];
 }
 
+Pizza.prototype.pizzaCost = function () {
+  var cost = 0.00;
+  switch (this.pizzaSize) {
+    case "small": cost += 8;
+      break;
+    case "medium": cost += 10;
+      break;
+    case "large": cost += 12;
+      break;
+    case "footlong": cost += 18;
+      break;
+  }
+  for (var i in this.toppings) {
+    cost += 5;
+  }
+  for (var i in this.cheeses) {
+    cost += 4;
+  }
+  for (var i in this.toppings) {
+    cost += 2;
+  }
+  for (var i in this.toppings) {
+    cost += 1;
+  }
+  return cost -= 0.01;
+}
+
 function Topping (toppingName) {
   this.toppingName = toppingName;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -7,6 +7,22 @@ describe('Pizza', function() {
       expect(pizza.salads).to.eql([]);
       expect(pizza.dressings).to.eql([]);
    });
+
+   it("returns final cost of pizza", function() {
+     var pizza = new Pizza("small");
+     var topping = new Topping("Pre-Cooked Bacon Pieces");
+     var firstCheese = new Cheese("Parmesan");
+     var secondCheese = new Cheese("Swiss");
+     var salad = new Salad("Beets");
+     var dressing = new Dressing("Beets");
+     pizza.toppings.push(topping);
+     pizza.cheeses.push(firstCheese);
+     pizza.cheeses.push(secondCheese);
+     pizza.salads.push(salad);
+     pizza.dressings.push(dressing);
+     expect(pizza.pizzaCost()).to.equal(23.99);
+
+  });
 });
 
 describe('Topping', function() {
