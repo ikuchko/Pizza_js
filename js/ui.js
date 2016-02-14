13f3917837a40c45b327f4998ae855541a7aab79
Illia <ikuchko@gmail.com>
2016-02-13 23:02:47 -0800

    finish styling UI.

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,4 @@
-var fillResultForm = function(order) {
+var fillOrderForm = function(order) {
   $("#total-form").append($("<div id='result'>"))
     for (var i in order.pizzas) {
       $("#result").append($("<p><strong>" + order.pizzas[i].pizzaSize + "</strong></p>"));
@@ -15,16 +15,32 @@ var fillResultForm = function(order) {
         $("#result").append($("<span> ♦ " + order.pizzas[i].dressings[dressingId] + "</span>"));
       }
       $("#result").append($("<span> ♦ </span>"));
+      $("#result").append($("<p class='cost'><strong>Price: $" + order.pizzas[i].pizzaCost() + "</strong></p>"));
       $("#result").append($("<hr>"));
     }
-    $("#total-form").append($("</div>"))
+    $("#total-form").append($("</div>"));
+}
+
+var fillTotalCostForm = function (order) {
+  var size = ["Small", "Medium", "Large", "One meter long"];
+  $("#total-form").append($("<div id='result'>"))
+  for (var i in size) {
+    if (order.totalAmount(size[i]) != 0) {
+      $("#result").append("<p>" + size[i] + " size: " + order.totalAmount(size[i]) + "</p>");
+    }
+  }
+  $("#result").append("<h4>Total cost: $" + order.totalCost() + "</h4>")
+                  .append("<h4>Thank you for your order, " + order.customer + "!</h4>")
+                  .append("<br>")
+                  .append('<button type="submit" class="btn btn-danger" id="new-order">New order</button>');
+  $("#total-form").append($("</div>"));
 }
 
 $(document).ready(function() {
   var order;
   $(".name-form").submit(function(event) {
     event.preventDefault();
-      order = new Order($("#customer").val(), parseInt($("#pizzaAmount").val()));
+      order = new Order($("#customer").val());
       $('#name-block').hide();
       $('#ingredients-block').show();
       $('#result-block').show();
@@ -45,13 +61,19 @@ $(document).ready(function() {
     pizza.dressings = $("#pizza-dressing").val();
     order.pizzas.push(pizza);
     $("#result").remove();
-    fillResultForm(order);
+    fillOrderForm(order);
+    $("#order-submit").show();
   });
 
+  $("#order-submit").click(function() {
+    if (order.totalCost() != -0.01) {
+      $("#result").remove();
+      fillTotalCostForm(order);
+      $("#order-submit").hide();
+    }
+    $("#new-order").click(function() {
+      location.reload();
+    });
+  });
 
-
-
-  // $("form#play-again").submit(function(event) {
-  //   location.reload(forceGet);
-  // });
 });
