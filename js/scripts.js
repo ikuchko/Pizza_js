75964ae8748af3a92b375cd78f4f27dbd4814cab
Illia <ikuchko@gmail.com>
2016-02-13 18:27:44 -0800

    add result form with preodered pizzas.

--- js/scripts.js
+++ js/scripts.js
@@ -38,38 +38,3 @@ function Order (customer, pizzaAmount) {
   this.pizzaAmount = pizzaAmount;
   this.pizzas = [];
 }
-
-var fillResultForm = function(order) {
-  $(".total-form").append($());
-}
-
-$(document).ready(function() {
-  var pizza = new Pizza;
-  $(".name-form").submit(function(event) {
-    event.preventDefault();
-      var order = new Order($("#customer").val(), parseInt($("#pizzaAmount").val()));
-      $('#name-block').hide();
-      $('#ingredients-block').show();
-  });
-
-  $("#ingredients-back").click(function() {
-    $('#ingredients-block').hide();
-    $('#name-block').show();
-  });
-
-  $("#ingredients-submit").click(function() {
-      pizza.pizzaSize = $("#pizza-size").val();
-      pizza.toppings = $("#pizza-topping").val();
-      pizza.cheeses = $("#pizza-cheese").val();
-      pizza.salads = $("#pizza-salad").val();
-      pizza.dressings = $("#pizza-dressing").val();
-      $('#ingredients-block').hide();
-      $('#result-block').show();
-  });
-
-
-
-  // $("form#play-again").submit(function(event) {
-  //   location.reload(forceGet);
-  // });
-});
--- js/ui.js
+++ js/ui.js
@@ -1,15 +1,57 @@
-
-
+var fillResultForm = function(order) {
+  $("#total-form").append($("<div id='result'>"))
+    for (var i in order.pizzas) {
+      $("#result").append($("<p><strong>" + order.pizzas[i].pizzaSize + "</strong></p>"));
+      for (var toppingId in order.pizzas[i].toppings) {
+        $("#result").append($("<span> ♦ " + order.pizzas[i].toppings[toppingId] + "</span>"));
+      }
+      for (var cheeseId in order.pizzas[i].cheeses) {
+        $("#result").append($("<span> ♦ " + order.pizzas[i].cheeses[cheeseId] + "</span>"));
+      }
+      for (var saladId in order.pizzas[i].salads) {
+        $("#result").append($("<span> ♦ " + order.pizzas[i].salads[saladId] + "</span>"));
+      }
+      for (var dressingId in order.pizzas[i].dressings) {
+        $("#result").append($("<span> ♦ " + order.pizzas[i].dressings[dressingId] + "</span>"));
+      }
+      $("#result").append($("<span> ♦ </span>"));
+      $("#result").append($("<hr>"));
+    }
+    $("#total-form").append($("</div>"))
+}
 
 $(document).ready(function() {
-  $("#name-submit").submit(function(event) {
+  var order;
+  $(".name-form").submit(function(event) {
     event.preventDefault();
-    $('#name-block').prop('title', 'your new title');
-    // document.getElementById('name-block').style = "display: none;";
-    // document.getElementById('name-size').style = "display: block;";
+      order = new Order($("#customer").val(), parseInt($("#pizzaAmount").val()));
+      $('#name-block').hide();
+      $('#ingredients-block').show();
+      $('#result-block').show();
   });
 
-  $("form#play-again").submit(function(event) {
-    location.reload(forceGet);
+  $("#ingredients-back").click(function() {
+    $('#ingredients-block').hide();
+    $('#result-block').hide();
+    $('#name-block').show();
   });
+
+  $("#ingredients-submit").click(function() {
+    var pizza = new Pizza;
+    pizza.pizzaSize = $("#pizza-size").val();
+    pizza.toppings = $("#pizza-topping").val();
+    pizza.cheeses = $("#pizza-cheese").val();
+    pizza.salads = $("#pizza-salad").val();
+    pizza.dressings = $("#pizza-dressing").val();
+    order.pizzas.push(pizza);
+    $("#result").remove();
+    fillResultForm(order);
+  });
+
+
+
+
+  // $("form#play-again").submit(function(event) {
+  //   location.reload(forceGet);
+  // });
 });
